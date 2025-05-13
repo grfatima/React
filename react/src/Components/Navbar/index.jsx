93ef9028bef9cd0634562056807af0b627c3c085
grfatime <kurbanovhafatime@gmail.com>
2025-05-13 21:19:37 +0400

    icon

--- react/src/Components/Navbar/index.jsx
+++ react/src/Components/Navbar/index.jsx
@@ -1,19 +1,21 @@
 import Links from "../../Mock/Links";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
+    <div className="flex items-center justify-between px-5">
+      <div className="flex items-center h-[80px] gap-5">
         <img src="/logo.png.svg" alt="logo" />
-      </div>
 
-      <div className="links">
         {Links.map(({ id, title, path }) => {
           return <p key={id}>{title}</p>;
         })}
+        {
+            
+        }
       </div>
 
-      <div className="button">
+      <div className="border-1 border-[#067A46] px-5 py-1 text-[#067A46]">
         <p>Log in</p>
       </div>
     </div>
--- react/src/Mock/Links.jsx
+++ react/src/Mock/Links.jsx
@@ -1,10 +1,32 @@
 const Links = [
   { id: 0, title: "Our Plans", path: "#" },
   { id: 1, title: "How It Works", path: "#" },
-  { id: 2, title: "Our Menus", path: "#" },
+  {
+    id: 2,
+    title: "Our Menus",
+    path: "#",
+    submenu: [
+      { id: a, title: "Breakfast", path: "#" },
+      { id: b, title: "Lunch", path: "#" },
+      { id: c, title: "Dinner", path: "#" },
+      { id: d, title: "Vegan", path: "#" },
+      { id: e, title: "Dessert", path: "#" },
+    ],
+  },
   { id: 3, title: "Gift Cards", path: "#" },
-  { id: 4, title: "Sustainability", path: "#" },
-  { id: 5, title: "Sustainability", path: "#" },
+  {
+    id: 4,
+    title: "Sustainability",
+    path: "#",
+    submenu: [
+      { id: aa, title: "Our Mission", path: "#" },
+      { id: bb, title: "Local Sourcing", path: "#" },
+      { id: cc, title: "Waste Reduction", path: "#" },
+      { id: dd, title: "Carbon Footprint", path: "#" },
+      { id: ee, title: "Get Involved", path: "#" },
+    ],
+  },
+  { id: 5, title: "Thanksgiving Box", path: "#" },
 ];
 
 export default Links;
